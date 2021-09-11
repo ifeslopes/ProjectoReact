e50535499cffd7628a248e664c31ac022391ce5f
ifeslopes <ifes.lopes@gmail.com>
2021-09-11 14:25:52 -0300

    project Create

---
+++ backend/src/test/java/com/projectreact/projectvendas/ProjectvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectreact.projectvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
