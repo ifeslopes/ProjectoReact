65a72732fc6e8d03d8b9603eef4d8f1c57487c78
ifeslopes <ifes.lopes@gmail.com>
2021-09-11 20:40:28 -0300

    teste git

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
         <h1 className="text-primary py-3"> Dashbord de vendas!</h1>
         <div className="row px-3">
           <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Taxa de sucesso(%)</h5>
+            <h5 className="text-center text-secondary">Taxa De Sucesso(%)</h5>
             <BarChar />
           </div>
           <div className="col-sm-6">
