91140083ad9377a0b092a1cd7b254d47f47dbab0
ifeslopes <ifes.lopes@gmail.com>
2021-09-12 22:35:01 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,44 +1,50 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts'
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
 
-type ChartData ={
-    labels:string[];
+type ChartData = {
+    labels: string[];
     series: number[];
 }
 const DonutChar = () => {
-//Formar Errada
-let chartData : ChartData = {labels:[], series:[]};
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
+    useEffect(() => {
+
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
+
+                setChartData({ labels: myLabels, series: mySeries });
+
+            })
+
+    }, [])
+
 
     //Formar Errada
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-    .then(response =>{
-        const data = response.data as SaleSum[];
-        const myLabels = data.map(x => x.sellerName);
-        const mySeries = data.map(x => x.sum);
 
-        chartData = {labels: myLabels, series:mySeries};
-        console.log(chartData)
-    })
 
     // const mockData = {
     //     series: [477138, 499928, 444867, 220426, 473088],
     //     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     // }
-    
+
     const options = {
         legend: {
             show: true
         }
     }
-    
+
     return (
         <Chart
-        options={{...options, labels:chartData.labels}}
-        series={chartData.series}
-        type="donut"
-        height="240"
+            options={{ ...options, labels: chartData.labels }}
+            series={chartData.series}
+            type="donut"
+            height="240"
 
         />
     );
